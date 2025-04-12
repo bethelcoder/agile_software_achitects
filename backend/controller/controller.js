b6694af5363f5a2e6eb1525465bfd77c13305e90
bethelcoder <2657187@students.wits.ac.za>
2025-04-12 04:22:21 +0200

    user form code

---
+++ backend/controller/controller.js
@@ -0,0 +1,13 @@
+//controllers for registration and login
+const regPage = (req, res) => {
+    res.render('register');
+}
+
+const logPage = (req, res) => {
+    res.render('login');
+}
+
+module.exports = {
+    regPage,
+    logPage
+}
