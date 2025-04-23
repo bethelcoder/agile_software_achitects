2fd5af8fbdf444326db048817e6b0f0b16228151
mrkhanyile21 <1858930@students.wits.ac.za>
2025-04-23 08:37:36 +0200

    Added all the necessary tests for the Project Model. Please check if they are running fine

---
+++ backend/tests/getProjectsByStatus.test.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const session = require("express-session");
+const request = require("supertest");
+
+// My own files
+const controller = require('../controller/controller');
+const project = require('../api/mongoDB/Project');
+const app = express();
+
+/**
+ * Mocks for:
+ * Project Model
+ * Session Middleware
+ * route testing
+ */
+
+jest.mock('../api/mongoDB/Project');
+
+describe ('These are tests for getProjectsByStatus', () =>{
+  let req, res;
+
+  beforeEach(() =>{
+    req ={
+      params: {
+        status: 'posted'
+      }
+    };
+    res = {
+      json: jest.fn(),
+      status: jest.fn().mockReturnThis()
+    };
+  });
+
+  // Test 1
+  it ("Should return projects with the given status", async () => {
+    const sampleProject = [
+      {title: 'Building a website'}
+    ];
+    project.find.mockResolvedValue(sampleProject);
+    await controller.getProjectsByStatus(req, res);
+
+    expect(project.find).toHaveBeenCalledWith({status: 'posted'});
+    expect(res.json).toHaveBeenCalledWith(sampleProject);
+
+  });
+
+  // Test 2
+  it ("Errors should be handled smoothly", async () => {
+    project.find.mockRejectedValue(new Error('DB error'));
+    await controller.getProjectsByStatus(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.json).toHaveBeenCalledWith({
+      error: 'Failed to get projects'
+    });
+
+  });
+});
