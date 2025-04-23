46f42cbcf57a06a6d808a34b1460d2c3187257db
mrkhanyile21 <1858930@students.wits.ac.za>
2025-04-23 08:58:47 +0200

    added test for the application model

--- backend/tests/getApplications.test.js
+++ backend/tests/getApplications.test.js
@@ -1,3 +1,53 @@
-test('true is true', () => {
-    expect(true).toBe(true);
+const express = require("express");
+const session = require("express-session");
+const request = require("supertest");
+
+// My own files
+const controller = require('../controller/controller');
+const application = require('../api/mongoDB/Application');
+const { populate } = require("../api/mongoDB/User");
+const app = express();
+
+/**
+ * Mocks for:
+ * Application Model
+ * Session Middleware
+ * route testing
+ */
+
+jest.mock('../api/mongoDB/Application');
+
+
+
+describe ('These are tests for createApplicationByFreelancer', () =>{
+  let req, res;
+
+  beforeEach(() =>{
+    req ={
+      params: {freelancerId: 'f2025'}
+    };
+    res = {
+      json: jest.fn(),
+      status: jest.fn().mockReturnThis()
+    };
   });
+
+  // Test 1
+  it ("Should get freelancer applications", async () => {
+    const createdApplication = [
+      {projectId: {title: 'p2025'}}
+    ];
+
+    application.find.mockReturnValue({
+      populate: jest.fn().mockResolvedValue(createdApplication)
+    });
+
+    await controller.getApplicationsByFreelancer(req, res);
+
+    expect(application.find).toHaveBeenCalledWith({
+      "freelancerId.userID": "f2025"
+    });
+    expect(res.json).toHaveBeenCalledWith(createdApplication);
+
+  });
+});
