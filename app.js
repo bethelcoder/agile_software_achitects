60589818df0b362c571365328bc61ebdbf8311b9
Tebogo Mabhele <123065694+bethelcoder@users.noreply.github.com>
2025-04-24 19:14:29 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT;
 require('dotenv').config();
 const mongoose = require('mongoose');
 const MongoStore = require('connect-mongo');
