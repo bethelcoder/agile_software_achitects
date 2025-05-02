342603e0ce23662182ea431cbb570b7f15b9dd5a
Ntobeko Mdakane <mdakanentobeko73@gmail.com>
2025-05-02 15:39:45 +0200

    Front-end Milestone management

--- backend/routes/projects.js
+++ backend/routes/projects.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Application = require('../api/mongoDB/Freelancer_Application');
+const ClientProject = require('../api/mongoDB/Project');
 const Project = require('../api/mongoDB/Freelancer_Project');
 const middleware = require('../middlewares');
 
@@ -17,6 +17,28 @@ function isFreelancer(req, res, next) {
 
 router.patch('/:projectId/accept', (req, res) => {
     res.send("Hello");
+});
+
+router.get('/activeProjects', middleware.ensureAuth, async (req, res) =>{
+    const clientId = req.session.user.userID;
+
+  try {
+    // Fetch all active projects by this client
+    const clientProjects = await ClientProject.find( {'clientID': clientId} );
+    if (clientProjects.length === 0) {
+      return res.render('activeProjects', { clientProjects: [] });
+    }
+      
+
+    res.render('activeprojects', { clientProjects });
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error fetching Projects.");
+  }
+});
+router.get('/reviewForm', (req, res) => {
+  res.render('reviewForm');
 });
 
 module.exports = router;
