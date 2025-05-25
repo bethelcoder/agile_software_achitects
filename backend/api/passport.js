4d2fbb7e4585430187b5cdf3be58ec12fc33fe3f
Struggler178 <2609046@students.wits.ac.za>
2025-05-25 20:01:50 +0200

    Update passport.js

--- backend/api/passport.js
+++ backend/api/passport.js
@@ -24,7 +24,7 @@ const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
     new GitHubStrategy({
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL:'http://localhost:4000/auth/github/callback',
+      callbackURL:'https://freelancestudioweb-f3dpbqgcf2d9dxdr.southafricanorth-01.azurewebsites.net/auth/github/callback',
     },
     (accessToken, refreshToken, profile, done) => {
         const user = {
